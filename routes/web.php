e3a38eeca24a582914b0acae9ef9e6c1cf2a4808
Abu Ubaida <ubaidanazia@gmail.com>
2024-06-23 05:46:54 +0600

    user role crud completed

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('permissions',PermissionController::class);
 Route::get('permissions/{permissionId}/delete',[PermissionController::class,'destroy']);
+
+Route::resource('roles',RoleController::class);
+Route::get('roles/{roleId}/delete',[RoleController::class,'destroy']);
 
 Route::get('/', function () {
     return view('welcome');
